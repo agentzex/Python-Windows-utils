87d64247be3ec640dc8318977e04f93a9941435c
Dor <39267196+dor-alt@users.noreply.github.com>
2018-07-08 23:48:02 +0300

    Add files via upload

---
+++ res_change/res_change.py
@@ -0,0 +1,13 @@
+from resolution_changer import revert_resolution, change_resolution
+
+
+if __name__ == "__main__":
+
+    # Use these functions to change the resolution of your monitor on Windows.
+    # change_resolution - change the display resolution to a given width and height parameters
+    # revert_resolution - reverts the display resolution back to its default parameters
+    # example:
+    width = 800
+    height = 600
+    #print change_resolution(width, height)
+    print revert_resolution()
